6a257031d898aeced36825fb8e7816d45abff37d
belen-mapplas <belen@mapplas.com>
2012-12-28 08:22:30 +0100

    Added scroll manager.

---
+++ src/com/mapplas/utils/infinite_scroll/InfiniteScrollManager.java
@@ -0,0 +1,51 @@
+package com.mapplas.utils.infinite_scroll;
+
+import java.util.ArrayList;
+
+import com.mapplas.model.App;
+
+public class InfiniteScrollManager {
+
+	private static int NUMBER_OF_APPS = 4;
+
+	private ArrayList<App> apps;
+
+	public InfiniteScrollManager(ArrayList<App> apps) {
+		this.apps = apps;
+	}
+
+	public ArrayList<App> getMore(int count) {
+		ArrayList<App> appsToReturn = new ArrayList<App>();
+		int pos = 0;
+
+		while (pos <= (count * InfiniteScrollManager.NUMBER_OF_APPS) - 1) {
+			if(count * InfiniteScrollManager.NUMBER_OF_APPS > this.apps.size()) {
+				// TODO: THINK!!
+			}
+			else {
+				appsToReturn.add(this.apps.get(pos));
+			}
+			pos++;
+		}
+
+		return appsToReturn;
+	}
+
+	public void getSobras(int count) {
+		int module = this.apps.size() % InfiniteScrollManager.NUMBER_OF_APPS;
+		if((count * InfiniteScrollManager.NUMBER_OF_APPS) + module > this.apps.size()) {
+
+		}
+
+	}
+
+	public int getMaxCount() {
+		if(this.apps.size() % InfiniteScrollManager.NUMBER_OF_APPS == 0) {
+			return this.apps.size() / InfiniteScrollManager.NUMBER_OF_APPS;
+		}
+		else {
+			return (this.apps.size() / InfiniteScrollManager.NUMBER_OF_APPS) + 1;
+		}
+	}
+
+}
