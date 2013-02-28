7824b6df5244f78c427ab628816c28c2358a1611
belen-mapplas <belen@mapplas.com>
2013-02-28 15:31:13 +0100

    Created custom WebView to allow doing wanted action when moving screen up.

--- src/com/mapplas/app/activities/WebViewActivity.java
+++ src/com/mapplas/app/activities/WebViewActivity.java
@@ -2,12 +2,10 @@ package com.mapplas.app.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.GestureDetector;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.webkit.WebView;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,25 +13,39 @@ import app.mapplas.com.R;
 
 import com.mapplas.app.application.MapplasApplication;
 import com.mapplas.model.App;
+import com.mapplas.utils.webView.TouchableWebView;
 
 public class WebViewActivity extends Activity {
 
 	private App app = null;
 	
 	private RelativeLayout navBar = null;
-
+	
+	private LinearLayout webLayout = null;
+	
+	private TouchableWebView gestureWebview = null;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.webview_layout);
+		
+		this.initializeNavigationBarAnimation();
+		this.extractDataFromBundle();
 
+		this.initializeTitle();
+		this.initializeBackButton();
+	}
+
+	private void extractDataFromBundle() {
 		Bundle extras = getIntent().getExtras();
 		if(extras != null) {
 			if(extras.containsKey(AppDetail.APP_DEV_URL_INTENT_DATA) && extras.getParcelable(AppDetail.APP_DEV_URL_INTENT_DATA) != null) {
 				this.app = (App)extras.getParcelable(AppDetail.APP_DEV_URL_INTENT_DATA);
 				if(!this.app.getAppUrl().equals("")) {
-					WebView myWebView = (WebView)findViewById(R.id.webview);
-					myWebView.loadUrl(this.app.getAppUrl());
+					this.gestureWebview.loadUrl(this.app.getAppUrl());
+//					this.gestureWebview.loadUrl("www.google.es");
+
 				}
 				else {
 					Toast errorToast = Toast.makeText(this, getString(R.string.app_detail_app_web_dev_url_error), Toast.LENGTH_LONG);
@@ -41,14 +53,6 @@ public class WebViewActivity extends Activity {
 				}
 			}
 		}
-
-		this.initializeTitle();
-		this.initializeBackButton();
-		
-		this.navBar = (RelativeLayout)findViewById(R.id.webview_navbar);
-		this.setTimerToNavigationBar();
-		
-//		GestureDetector gestureDetector = new GestureDetect
 	}
 
 	private void initializeTitle() {
@@ -73,14 +77,13 @@ public class WebViewActivity extends Activity {
 			}
 		});
 	}
-
-	private void setTimerToNavigationBar() {
-		this.navBar.postDelayed(new Runnable() {
-			@Override
-			public void run() {
-				navBar.setVisibility(View.GONE);
-			}
-		}, 3000);
-	}
 	
+	private void initializeNavigationBarAnimation() {
+		this.webLayout = (LinearLayout)findViewById(R.id.webview_layout);
+		this.navBar = (RelativeLayout)findViewById(R.id.webview_navbar);
+		
+		this.gestureWebview = new TouchableWebView(this);
+		this.gestureWebview.setNavBar(this.navBar);
+		this.webLayout.addView(gestureWebview);
+	}
 }
---
+++ src/com/mapplas/utils/webView/TouchableWebView.java
@@ -0,0 +1,52 @@
+package com.mapplas.utils.webView;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.RelativeLayout;
+
+public class TouchableWebView extends WebView {
+
+	private RelativeLayout navBar = null;
+
+	public TouchableWebView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+	}
+
+	public TouchableWebView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public TouchableWebView(Context context) {
+		super(context);
+	}
+
+	public void setNavBar(RelativeLayout navigationBar) {
+		this.navBar = navigationBar;
+		this.setTimerToNavigationBar();
+	}
+
+	private void setTimerToNavigationBar() {
+		this.navBar.postDelayed(new Runnable() {
+
+			@Override
+			public void run() {
+				navBar.setVisibility(View.GONE);
+			}
+		}, 3000);
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		switch (event.getAction()) {
+			case (MotionEvent.ACTION_DOWN):
+				this.navBar.setVisibility(View.VISIBLE);
+				this.setTimerToNavigationBar();
+				return true;
+			default:
+				return false;
+		}
+	}
+}
