c3445f896bebffe2272a9650bf77bdc6b1b09282
belen-mapplas <belen@mapplas.com>
2013-03-02 11:58:25 +0100

    Added processing dialog while web-page is being loaded.

--- src/com/mapplas/app/activities/WebViewActivity.java
+++ src/com/mapplas/app/activities/WebViewActivity.java
@@ -1,14 +1,14 @@
 package com.mapplas.app.activities;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
-import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 import app.mapplas.com.R;
@@ -16,7 +16,6 @@ import app.mapplas.com.R;
 import com.mapplas.app.application.MapplasApplication;
 import com.mapplas.model.App;
 import com.mapplas.utils.webView.TouchableScrollView;
-import com.mapplas.utils.webView.TouchableWebView;
 
 public class WebViewActivity extends Activity {
 
@@ -25,19 +24,32 @@ public class WebViewActivity extends Activity {
 	private RelativeLayout navBar = null;
 
 	private TouchableScrollView scroll = null;
-
-	private TouchableWebView gestureWebview = null;
+	
+	private WebView webView = null;
+	
+	private ProgressDialog progressDialog = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.webview_layout);
 
-		this.initializeNavigationBarAnimation();
 		this.extractDataFromBundle();
+		this.setWebViewClientToWebView();
 
+		this.initializeComponents();
 		this.initializeTitle();
 		this.initializeBackButton();
+	}
+
+	private void setWebViewClientToWebView() {
+		this.webView.setWebViewClient(new WebViewClient() {
+			@Override
+			public void onPageFinished(WebView view, String url) {
+				super.onPageFinished(view, url);
+				scroll.stopDialogAndStartAnimation();
+			}
+		});
 	}
 
 	private void extractDataFromBundle() {
@@ -46,9 +58,8 @@ public class WebViewActivity extends Activity {
 			if(extras.containsKey(AppDetail.APP_DEV_URL_INTENT_DATA) && extras.getParcelable(AppDetail.APP_DEV_URL_INTENT_DATA) != null) {
 				this.app = (App)extras.getParcelable(AppDetail.APP_DEV_URL_INTENT_DATA);
 				if(!this.app.getAppUrl().equals("")) {
-					this.gestureWebview.loadUrl(this.app.getAppUrl());
-					// this.gestureWebview.loadUrl("www.google.es");
-
+					this.webView = (WebView)findViewById(R.id.webView);
+					this.webView.loadUrl(this.app.getAppUrl());
 				}
 				else {
 					Toast errorToast = Toast.makeText(this, getString(R.string.app_detail_app_web_dev_url_error), Toast.LENGTH_LONG);
@@ -81,14 +92,13 @@ public class WebViewActivity extends Activity {
 		});
 	}
 
-	private void initializeNavigationBarAnimation() {
+	private void initializeComponents() {
 		this.scroll = (TouchableScrollView)findViewById(R.id.scrollView1);
+		
 		this.navBar = (RelativeLayout)findViewById(R.id.webview_navbar);
-
-		this.gestureWebview = new TouchableWebView(this);
-		this.gestureWebview.setNavBar(this.navBar);
 		this.scroll.setNavBar(this.navBar);
-		this.scroll.addView(gestureWebview);
+		
+		this.progressDialog = ProgressDialog.show(this, "", "Please wait...");
+		this.scroll.setProgressDialog(this.progressDialog);
 	}
-
 }
--- src/com/mapplas/utils/webView/TouchableScrollView.java
+++ src/com/mapplas/utils/webView/TouchableScrollView.java
@@ -1,14 +1,24 @@
 package com.mapplas.utils.webView;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 
 public class TouchableScrollView extends ScrollView {
 
-	private RelativeLayout navBar = null;
+	private RelativeLayout navBar;
+	
+	private ProgressDialog progressDialog;
+
+	private Animation translateOutAnimation;
+
+	private Animation translateInAnimation;
 
 	public TouchableScrollView(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
@@ -24,12 +34,22 @@ public class TouchableScrollView extends ScrollView {
 
 	public void setNavBar(RelativeLayout navigationBar) {
 		this.navBar = navigationBar;
+		this.initAnimations();
+	}
+	
+	public void setProgressDialog(ProgressDialog progressDialog) {
+		this.progressDialog = progressDialog;
+	}
+	
+	public void stopDialogAndStartAnimation() {
+		this.progressDialog.hide();
+		this.setTimerToNavigationBar();
 	}
 
 	@Override
 	protected void onScrollChanged(int l, int t, int oldl, int oldt) {
 		if(t <= 10) {
-			this.navBar.setVisibility(View.VISIBLE);
+			// this.navBar.startAnimation(this.translateInAnimation);
 			this.setTimerToNavigationBar();
 		}
 		super.onScrollChanged(l, t, oldl, oldt);
@@ -40,9 +60,53 @@ public class TouchableScrollView extends ScrollView {
 
 			@Override
 			public void run() {
+				navBar.startAnimation(translateOutAnimation);
+			}
+		}, 9000);
+	}
+
+	private void initAnimations() {
+		this.translateOutAnimation = new AlphaAnimation(1, 0);
+		this.translateOutAnimation.setInterpolator(new AccelerateInterpolator()); // and
+		this.translateOutAnimation.setStartOffset(0);
+		this.translateOutAnimation.setDuration(500);
+		this.translateOutAnimation.setAnimationListener(new Animation.AnimationListener() {
+
+			@Override
+			public void onAnimationStart(Animation animation) {
+			}
+
+			@Override
+			public void onAnimationRepeat(Animation animation) {
+			}
+
+			@Override
+			public void onAnimationEnd(Animation animation) {
 				navBar.setVisibility(View.GONE);
 			}
-		}, 3000);
+		});
+
+		// this.translateInAnimation = new TranslateAnimation(0, 0, -100, 0);
+		// this.translateInAnimation.setInterpolator(new
+		// AccelerateInterpolator());
+		// this.translateInAnimation.setStartOffset(0);
+		// this.translateInAnimation.setDuration(700);
+		// this.translateOutAnimation.setAnimationListener(new
+		// Animation.AnimationListener() {
+		//
+		// @Override
+		// public void onAnimationStart(Animation animation) {
+		// navBar.setVisibility(View.VISIBLE);
+		// }
+		//
+		// @Override
+		// public void onAnimationRepeat(Animation animation) {
+		// }
+		//
+		// @Override
+		// public void onAnimationEnd(Animation animation) {
+		// }
+		// });
 	}
 
 }
