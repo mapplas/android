3214d14b3265babbf6c7384ccde9a724f204a1d4
belen-mapplas <belen@mapplas.com>
2013-08-22 09:21:37 +0200

    Added basque string literals.

--- src/com/mapplas/app/activities/AboutUsActivity.java
+++ src/com/mapplas/app/activities/AboutUsActivity.java
@@ -3,6 +3,7 @@ package com.mapplas.app.activities;
 import java.util.Locale;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.graphics.Typeface;
 import android.os.Bundle;
@@ -83,6 +84,13 @@ public class AboutUsActivity extends Activity implements LanguageDialogInterface
         Configuration config = new Configuration();
         config.locale = locale;
         getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+        
+        Intent intent = getIntent();
+        overridePendingTransition(0, 0);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        finish();
+        overridePendingTransition(0, 0);
+        startActivity(intent);
 	}
 
 }
--- src/com/mapplas/utils/utils/DateUtils.java
+++ src/com/mapplas/utils/utils/DateUtils.java
@@ -1,12 +1,5 @@
 package com.mapplas.utils.utils;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import android.content.Context;
-import app.mapplas.com.R;
-
 public class DateUtils {
 
 	public static int Hours(String hour) {
@@ -33,69 +26,4 @@ public class DateUtils {
 		return ret;
 	}
 
-	public static String FormatSinceDate(String date, String hour, Context context) {
-		String ret = date + " " + hour; // Fallback
-		int aux = 0;
-		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm");
-		Date now = new Date();
-		Date past;
-		try {
-			past = df.parse(date + "T" + hour);
-
-			if(now.getYear() > past.getYear()) {
-				// Hace X años
-				aux = now.getYear() - past.getYear();
-				if(aux == 1) {
-					ret = aux + " " + context.getString(R.string.year);
-				}
-				else {
-					ret = aux + " " + context.getString(R.string.years);
-				}
-			}
-			else if(now.getMonth() > past.getMonth()) {
-				// Hace X meses
-				aux = now.getMonth() - past.getMonth();
-				if(aux == 1) {
-					ret = aux + " " + context.getString(R.string.month);
-				}
-				else {
-					ret = aux + " " + context.getString(R.string.months);
-				}
-			}
-			else if(now.getDate() > past.getDate()) {
-				// Hace X dias
-				aux = now.getDate() - past.getDate();
-				if(aux == 1) {
-					ret = aux + " " + context.getString(R.string.day);
-				}
-				else {
-					ret = aux + " " + context.getString(R.string.days);
-				}
-			}
-			else if(now.getHours() > past.getHours()) {
-				// Hace X horas
-				aux = now.getHours() - past.getHours();
-				if(aux == 1) {
-					ret = aux + " " + context.getString(R.string.hour);
-				}
-				else {
-					ret = aux + " " + context.getString(R.string.hours);
-				}
-			}
-			else {
-				// Hace X minutos
-				aux = now.getMinutes() - past.getMinutes();
-				if(aux == 1) {
-					ret = aux + " " + context.getString(R.string.minute);
-				}
-				else {
-					ret = aux + " " + context.getString(R.string.minutes);
-				}
-			}
-
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
-		return ret;
-	}
 }
