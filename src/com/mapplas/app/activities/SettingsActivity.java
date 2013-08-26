da5d38ab8df7c9dacce2fcfa949035ec44037ac2
belen-mapplas <belen@mapplas.com>
2013-08-26 11:16:36 +0200

    After changing language, a dialog is shown to inform user about application restart.

--- src/com/mapplas/app/activities/SettingsActivity.java
+++ src/com/mapplas/app/activities/SettingsActivity.java
@@ -3,12 +3,16 @@ package com.mapplas.app.activities;
 import java.util.Locale;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.content.DialogInterface.OnKeyListener;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ListView;
 import app.mapplas.com.R;
@@ -26,7 +30,7 @@ public class SettingsActivity extends Activity implements LanguageDialogInterfac
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.preferences);
-		
+
 		this.initializeLayoutComponents();
 	}
 
@@ -34,7 +38,7 @@ public class SettingsActivity extends Activity implements LanguageDialogInterfac
 		Typeface normalTypeFace = ((MapplasApplication)this.getApplicationContext()).getTypeFace();
 
 		Button backButton = (Button)this.findViewById(R.id.preferences_back_button);
-		backButton.setOnClickListener(new OnClickListener() {
+		backButton.setOnClickListener(new View.OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
@@ -52,23 +56,26 @@ public class SettingsActivity extends Activity implements LanguageDialogInterfac
 	public void onDialogEnglishLanguageClick() {
 		((MapplasApplication)this.getApplicationContext()).setLanguage(Constants.ENGLISH);
 		updateLanguage(((MapplasApplication)this.getApplicationContext()).getLanguage());
+		this.showAppRestartDialog();
 	}
 
 	@Override
 	public void onDialogSpanishLanguageClick() {
 		((MapplasApplication)this.getApplicationContext()).setLanguage(Constants.SPANISH);
 		updateLanguage(((MapplasApplication)this.getApplicationContext()).getLanguage());
+		this.showAppRestartDialog();
 	}
 
 	@Override
 	public void onDialogBasqueLanguageClick() {
 		((MapplasApplication)this.getApplicationContext()).setLanguage(Constants.BASQUE);
 		updateLanguage(((MapplasApplication)this.getApplicationContext()).getLanguage());
+		this.showAppRestartDialog();
 	}
 
 	private void updateLanguage(String language) {
 		this.adapter.notifyDataSetChanged();
-		
+
 		Locale locale = null;
 		if(language.equals(Constants.ENGLISH)) {
 			locale = new Locale("en");
@@ -84,11 +91,33 @@ public class SettingsActivity extends Activity implements LanguageDialogInterfac
 		Configuration config = new Configuration();
 		config.locale = locale;
 		getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
-
-		Intent intent = new Intent(this, MapplasActivity.class);
-		intent.putExtra(Constants.SETTINGS_LANGUAGE_CHANGE_BUNDLE, true);
-		this.startActivity(intent);
-		this.finish();
 	}
 
+	private void showAppRestartDialog() {
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setMessage(R.string.language_change_restart_app_message);
+		builder.setPositiveButton(R.string.accept, new OnClickListener() {
+
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				dialog.dismiss();
+				Intent intent = new Intent(SettingsActivity.this, MapplasActivity.class);
+				intent.putExtra(Constants.SETTINGS_LANGUAGE_CHANGE_BUNDLE, true);
+				startActivity(intent);
+				finish();
+			}
+		});
+
+		AlertDialog dialog = builder.create();
+		
+		dialog.setOnKeyListener(new OnKeyListener() {
+			
+			@Override
+			public boolean onKey(DialogInterface dialog, int keyCode, KeyEvent event) {
+				return true;
+			}
+		});
+		
+		dialog.show();
+	}
 }
